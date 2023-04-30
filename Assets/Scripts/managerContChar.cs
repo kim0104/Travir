a41a6d41b67923ee4a31a0ded4952fb5f14b6800
RachelPark012 <113976009+rachel0402@users.noreply.github.com>
2023-04-30 17:53:53 +0900

    feat. player 움직임, 회전 기능 추가

--- Assets/Scripts/managerContChar.cs
+++ Assets/Scripts/managerContChar.cs
@@ -7,20 +7,26 @@ public class managerContChar : MonoBehaviour
     private CharacterController _charController;
     private Animator _animator;
 
+    //
+    private Transform meshPlayer;
+
     //move
     private float inputX;
     private float inputZ;
     private Vector3 v_movement;
     private float moveSpeed;
+    private float gravity;
 
 
     // Start is called before the first frame update
     void Start()
     {
         moveSpeed = 0.1f;
+        gravity = 0.5f;
         GameObject tempPlayer = GameObject.FindGameObjectWithTag("Player");
+        meshPlayer = tempPlayer.transform.GetChild(0);
         _charController = tempPlayer.GetComponent<CharacterController>();
-        _animator = tempPlayer.GetComponent<Animator>();
+        _animator = meshPlayer.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -43,7 +49,26 @@ public class managerContChar : MonoBehaviour
 
     private void FixedUpdate()
     {
-        v_movement = new Vector3 (inputX*moveSpeed,0,inputZ*moveSpeed);
+        //gravity
+        if(_charController.isGrounded)
+        {
+            v_movement.y = 0f;
+        }
+        else
+        {
+            v_movement.y -= gravity * Time.deltaTime;
+        }
+
+        //movement
+        v_movement = new Vector3 (inputX*moveSpeed,v_movement.y ,inputZ*moveSpeed);
         _charController.Move(v_movement);
+
+        //mesh rotate
+        if (inputX !=0 || inputZ !=0)
+        {
+            Vector3 lookDir = new Vector3(v_movement.x,0,v_movement.z);
+            meshPlayer.rotation = Quaternion.LookRotation(lookDir);
+        }
+        
     }
 }
