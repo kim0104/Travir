a93fe61bc08cd96c04885d3b599d264f47c708a1
syes2 <purin298_@naver.com>
2023-06-04 04:02:30 +0900

    Fix. modify dialogue system
    NPC 대화창 수정
    
    그림자 관련 수정해야됨

--- Assets/02.Scripts/NPC/NPCRotation.cs
+++ Assets/02.Scripts/NPC/NPCRotation.cs
@@ -3,10 +3,18 @@ using UnityEngine;
 public class NPCRotation : MonoBehaviour
 {
 
+    public Transform targetObject;
+
     public void RotateNPC(float angle)
     {
         Vector3 currentRotation = transform.eulerAngles;
         currentRotation.y = angle;
         transform.eulerAngles = currentRotation;
+    }
+
+    public void SetNPCPosition()
+    {
+        Vector3 newPosition = new Vector3(271f, 13f, 137f); // 변경하고자 하는 새로운 위치 좌표
+        targetObject.position = newPosition;
     }
 }
