c20e12e3314ce18420d69f0c73221d5fedd2b2a8
unknown <judy245@sookmyung.ac.kr>
2023-07-06 02:03:51 +0900

    feat&fix: add npc in Seoul, fix reviewcanvasactive
    - 서울 씬에 reviewNPC 추가
    - 서울 씬에서 reviewcanvas가 켜지지 않는 오류 해결

--- Assets/02.Scripts/Review/ReviewCanvasActive.cs
+++ Assets/02.Scripts/Review/ReviewCanvasActive.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿/*
+using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
@@ -19,6 +20,49 @@ public class ReviewCanvasActive : MonoBehaviour, IPointerClickHandler
     }
 
     public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("click");
+        if (!canvasShown && canInteract)
+        {
+            reviewCanvas.SetActive(true);
+            reviewlistScreen.SetActive(true);
+            addreviewScreen.SetActive(false);
+            canvasShown = true;
+            canInteract = false;
+        }
+    }
+
+    private void CloseCanvas()
+    {
+        reviewCanvas.SetActive(false);
+        canvasShown = false;
+        canInteract = true;
+    }
+}
+
+
+*/
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ReviewCanvasActive : MonoBehaviour
+{
+    public GameObject reviewCanvas;
+    public GameObject reviewlistScreen;
+    public GameObject addreviewScreen;
+    public Button closeButton;
+
+    private bool canvasShown = false;
+    private bool canInteract = true;
+
+    private void Start()
+    {
+        reviewCanvas.SetActive(false);
+        closeButton.onClick.AddListener(CloseCanvas);
+    }
+
+    private void OnMouseUp()
     {
         if (!canvasShown && canInteract)
         {
