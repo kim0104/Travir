a1d39138693995a4003270919a763754a5bd8ec9
rachel0402 <113976009+rachel0402@users.noreply.github.com>
2023-09-13 18:08:31 +0900

    feat. seoul landmark 상세보기&플레이어 움직임 수정&스크립트 정리

---
+++ Assets/02.Menu Scripts/MenuPanel.cs
@@ -0,0 +1,170 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine.SceneManagement;
+using Photon.Realtime;
+
+public class MenuPanel : MonoBehaviour
+{
+    public GameObject settingsPanel; // Settings Panel GameObject
+    public GameObject mapPanel; // Map Panel GameObject
+    public Slider volumeSlider; // Volume Slider
+    public Button quitButton; // Quit Button
+    public Button mapButton; // map Button
+    public Button gimpoButton;
+    public Button lotteworldButton;
+    public Button lottetowerButton;
+    public Button namsanButton;
+    public Button gwanghwaButton;
+    public Button worldcupButton;
+    public Button settingscloseButton; // Settings Panel close
+    public Button mapcloseButton; // Map Panel close
+    public Button returnButton; // return to Settings Panel
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Add listener for volume slider
+        volumeSlider.onValueChanged.AddListener(delegate { OnVolumeChange(); });
+
+        // Add listener for quit button
+        quitButton.onClick.AddListener(delegate { OnQuitClick(); });
+
+        // Add listener for map button
+        mapButton.onClick.AddListener(delegate { OnMapClick(); });
+
+        gimpoButton.onClick.AddListener(delegate { OnGimpoClick(); });
+
+        lotteworldButton.onClick.AddListener(delegate { OnLotteworldClick(); });
+
+        lottetowerButton.onClick.AddListener(delegate { OnLottetowerClick(); });
+
+        namsanButton.onClick.AddListener(delegate { OnNamsanClick(); });
+
+        gwanghwaButton.onClick.AddListener(delegate { OnGwanghwaClick(); });
+
+        worldcupButton.onClick.AddListener(delegate { OnWorldCupClick(); });
+
+        settingscloseButton.onClick.AddListener(delegate { OnSettigsCloseClick(); });
+
+        mapcloseButton.onClick.AddListener(delegate { OnMapCloseClick(); });
+
+        returnButton.onClick.AddListener(delegate { OnReturnClick(); });
+
+        // Hide the settings panel at start
+        settingsPanel.SetActive(false);
+        mapPanel.SetActive(false);
+
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If the settings button (assumed to be 'S' key here) is pressed, toggle the settings panel
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            settingsPanel.SetActive(!settingsPanel.activeSelf);
+        }
+    }
+
+    // Called when the volume slider value is changed
+    void OnVolumeChange()
+    {
+        // Set the volume of the AudioListener (i.e., the global volume) to the slider value
+        AudioListener.volume = volumeSlider.value;
+    }
+
+    // Called when the quit button is clicked
+    void OnQuitClick()
+    {
+        // Quit the application
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+    void OnMapClick()
+    {
+        //Map Panel change
+        settingsPanel.SetActive(false);
+        mapPanel.SetActive(true);
+
+    }
+
+
+    public SeoulMapManager seoulMapManager; // SeoulMapManager ÂüÁ¶
+    void OnGimpoClick()
+    {
+        Data.spawnType = Data.SpawnType.Gimpo;
+        GameObject player = GameObject.FindGameObjectWithTag("Player"); // ÇÃ·¹ÀÌ¾î ÅÂ±×¸¦ »ç¿ëÇÏ¿© ÇÃ·¹ÀÌ¾î °´Ã¼¸¦ Ã£½À´Ï´Ù.
+        seoulMapManager.ChangePlayerPosition(player);
+        mapPanel.SetActive(false);
+    }
+
+    void OnLotteworldClick()
+    {
+        Data.spawnType = Data.SpawnType.Lotteworld;
+        GameObject player = GameObject.FindGameObjectWithTag("Player"); // ÇÃ·¹ÀÌ¾î ÅÂ±×¸¦ »ç¿ëÇÏ¿© ÇÃ·¹ÀÌ¾î °´Ã¼¸¦ Ã£½À´Ï´Ù.
+        seoulMapManager.ChangePlayerPosition(player);
+        mapPanel.SetActive(false); ;
+    }
+
+    void OnLottetowerClick()
+    {
+        Data.spawnType = Data.SpawnType.Lottetower;
+        GameObject player = GameObject.FindGameObjectWithTag("Player"); // ÇÃ·¹ÀÌ¾î ÅÂ±×¸¦ »ç¿ëÇÏ¿© ÇÃ·¹ÀÌ¾î °´Ã¼¸¦ Ã£½À´Ï´Ù.
+        seoulMapManager.ChangePlayerPosition(player);
+        mapPanel.SetActive(false);
+    }
+
+    void OnNamsanClick()
+    {
+        Data.spawnType = Data.SpawnType.Namsan;
+        GameObject player = GameObject.FindGameObjectWithTag("Player"); // ÇÃ·¹ÀÌ¾î ÅÂ±×¸¦ »ç¿ëÇÏ¿© ÇÃ·¹ÀÌ¾î °´Ã¼¸¦ Ã£½À´Ï´Ù.
+        seoulMapManager.ChangePlayerPosition(player);
+        mapPanel.SetActive(false);
+    }
+
+    void OnGwanghwaClick()
+    {
+        Data.spawnType = Data.SpawnType.Gwanghwa;
+        GameObject player = GameObject.FindGameObjectWithTag("Player"); // ÇÃ·¹ÀÌ¾î ÅÂ±×¸¦ »ç¿ëÇÏ¿© ÇÃ·¹ÀÌ¾î °´Ã¼¸¦ Ã£½À´Ï´Ù.
+        seoulMapManager.ChangePlayerPosition(player);
+        mapPanel.SetActive(false);
+    }
+
+    void OnWorldCupClick()
+    {
+        Data.spawnType = Data.SpawnType.Worldcup;
+        GameObject player = GameObject.FindGameObjectWithTag("Player"); // ÇÃ·¹ÀÌ¾î ÅÂ±×¸¦ »ç¿ëÇÏ¿© ÇÃ·¹ÀÌ¾î °´Ã¼¸¦ Ã£½À´Ï´Ù.
+        seoulMapManager.ChangePlayerPosition(player);
+        mapPanel.SetActive(false);
+    }
+
+
+    void OnSettigsCloseClick()
+    {
+        settingsPanel.SetActive(false);
+    }
+
+    void OnMapCloseClick()
+    {
+        mapPanel.SetActive(false);
+    }
+
+    void OnReturnClick()
+    {
+        mapPanel.SetActive(false);
+        settingsPanel.SetActive(true);
+    }
+
+    
+}
+
---
+++ Assets/02.Scripts/MovedURL.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovedURL : MonoBehaviour
+{
+    public void LotteWorld()
+    {
+        Application.OpenURL("https://korean.visitkorea.or.kr/detail/ms_detail.do?cotid=78a00ac3-ad3c-4637-8629-48aefb22a79b&big_category=A02&mid_category=A0202&big_area=1"); // 대한민국 구석구석_롯데월드
+    }
+
+    public void LotteTower()
+    {
+        Application.OpenURL("https://korean.visitkorea.or.kr/detail/ms_detail.do?cotid=762e796f-d214-4d79-b777-c6183cef1c73&big_category=A02&mid_category=A0205&big_area=1"); // 대한민국 구석구석_롯데월드타워
+    }
+
+    public void WorldCup()
+    {
+        Application.OpenURL("https://korean.visitkorea.or.kr/detail/ms_detail.do?cotid=5aa3f35b-ec93-4cbf-97d6-4e24b588893b&big_category=A03&mid_category=A0302&big_area=1"); // 대한민국 구석구석_서울월드컵경기장
+    }
+
+    public void Gwanghwa()
+    {
+        Application.OpenURL("https://korean.visitkorea.or.kr/detail/ms_detail.do?cotid=df1633eb-9b60-402e-8d2e-31114f6382c4&big_category=A02&mid_category=A0202&big_area=1"); // 대한민국 구석구석_광화문광장
+    }
+
+    public void Namsan()
+    {
+        Application.OpenURL("https://korean.visitkorea.or.kr/detail/ms_detail.do?cotid=ad931739-fca3-4135-a878-cc7fb82920ba"); // 대한민국 구석구석_남산
+    }
+}
